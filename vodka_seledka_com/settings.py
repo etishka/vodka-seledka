90042962554e9e573501cc6dc43d74ffbf5fe14b
Alexander Smirnov <alexander.smirnoff@gmail.com>
2016-08-11 17:46:05 -0700

    Update

--- vodka_seledka_com/settings.py
+++ vodka_seledka_com/settings.py
@@ -23,19 +23,27 @@ SECRET_KEY = '7!0fur8brb_s0ft()2q)!cxsztso%d9rej%0*&u^zvmj^(*=z1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+#                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
     },
 ]
 
 # TEMPLATE_DIRS = (
 #     "main/templates",
 # )
-ALLOWED_HOSTS = [".herokuapp.com", ".vodka-seledka.com"]
+ALLOWED_HOSTS = [".herokuapp.com", ".vodka-seledka.com", "127.0.0.1"]
 
 
 # Application definition
